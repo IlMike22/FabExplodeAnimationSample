0150549b443cac1939191e0eb23b350943847610
IlMike22 <michaelwidlok@gmail.com>
2024-12-26 13:06:36 +0100

    add fab animation

---
+++ app/src/main/java/de/mindmarket/fabexplodeanimationsample/MainActivity.kt
@@ -0,0 +1,83 @@
+package de.mindmarket.fabexplodeanimationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import de.mindmarket.fabexplodeanimationsample.ui.theme.FabExplodeAnimationSampleTheme
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object MainRoute
+
+@Serializable
+data object AddItemRoute
+
+const val FAB_EXPLODE_BOUNDS_KEY = "FAB_EXPLODE_BOUNDS_KEY"
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FabExplodeAnimationSampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+                    val fabColor = Color.Green
+                    SharedTransitionLayout {
+                        NavHost(
+                            navController = navController,
+                            startDestination = MainRoute,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                                .consumeWindowInsets(innerPadding)
+                        ) {
+                            composable<MainRoute> {
+                                MainScreen(
+                                    fabColor = fabColor,
+                                    animatedVisibilityScope = this,
+                                    onFabClick = {
+                                        navController.navigate(AddItemRoute)
+                                    }
+                                )
+                            }
+                            composable<AddItemRoute> {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .background(fabColor)
+                                        .sharedBounds(
+                                            sharedContentState = rememberSharedContentState(
+                                                FAB_EXPLODE_BOUNDS_KEY
+                                            ),
+                                            animatedVisibilityScope = this
+                                        ),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(text = "Add item")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
